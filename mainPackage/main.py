4769b51935944e186f87b3f79369ef9185f43b57
Carto <Carto@AnnaLaptop.wifi.uc.edu>
2024-04-18 15:14:05 -0400

    Our location is all in one sentence rather than a list

--- mainPackage/main.py
+++ mainPackage/main.py
@@ -5,5 +5,4 @@ from campusPackage.campus import *
 if __name__ == "__main__":
     fox_words = decode_fox_category("UCEnglish.txt", "EncryptedGroupHints Spring 2024 Section 001-1.json")
     print("Our location on campus is:")
-    for word in fox_words:
-        print(word)
+    print(' '.join(fox_words))
