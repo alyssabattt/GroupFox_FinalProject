68d69a19798529048d4d766450b9827146f4405b
duncanward <duncanward@duncans-mbp.lan>
2024-04-21 20:29:25 -0400

    movie,decoded:issue with pic

--- mainPackage/main.py
+++ mainPackage/main.py
@@ -5,10 +5,12 @@
 
 from campusPackage.campus import *
 from PIL import Image 
+from campusPackage.movie import *
 
 if __name__ == "__main__":
     fox_words = decode_fox_category("UCEnglish.txt", "EncryptedGroupHints Spring 2024 Section 001-1.json")
     print("Our location on campus is:")
     print(' '.join(fox_words))
-    image= Image.open('bcPic.png')
+    image= Image.open('groupPic.png')
     image.show()
+    
